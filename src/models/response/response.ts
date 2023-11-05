3df96c0e2f137bcaed76e7897ea874ddcb8dec4c
Toodie <lucas.vtodt@gmail.com>
2023-11-05 11:42:49 -0300

    add model that represents a response

---
+++ src/models/response/response.ts
@@ -0,0 +1,14 @@
+import { HttpStatus } from "@nestjs/common";
+import { ResponseMessages } from "src/enums/response/messages/response.messages";
+
+export class Response<Payload> {
+    httpStatus: HttpStatus
+    message: ResponseMessages
+    payload: Payload
+
+    constructor(httpStatus: HttpStatus, message: ResponseMessages, payload: Payload) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+        this.payload = payload;
+    }
+}

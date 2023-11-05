4cfeebe756c6242ee8619b6f64fb97caf32cdb71
Toodie <lucas.vtodt@gmail.com>
2023-11-05 12:08:55 -0300

    move services to endpoints folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CacheModule } from '@nestjs/cache-manager';
-import { TokensModule } from './tokens/tokens.module';
-import { RegistriesModule } from './registries/registries.module';
+import { TokensModule } from './endpoints/tokens/tokens.module';
+import { RegistriesModule } from './endpoints/registries/registries.module';
 import * as redisStore from 'cache-manager-redis-store';
 import { ResponseGeneratorUtil } from './utils/response-generator/response-generator.util';
 

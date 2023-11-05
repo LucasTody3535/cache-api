4f53ebe9c731c7e70993cc4ceff60a172eb43141
Toodie <lucas.vtodt@gmail.com>
2023-11-05 11:42:24 -0300

    add enums for response messages

---
+++ src/enums/response/messages/response.messages.ts
@@ -0,0 +1,6 @@
+export enum ResponseMessages {
+    DATA_RETRIEVED = "Data was succesfully retrieved!",
+    DATA_NOT_RETRIEVED = "Data was NOT succesfully retrieved!",
+    DATA_SAVED = "Data was succesfully saved!",
+    DATA_NOT_SAVED = "Data was NOT succesfully saved!"
+}

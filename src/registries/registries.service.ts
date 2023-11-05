2d9f340a00994830a167c0a0cfd27be73b4f0fd4
Toodie <lucas.vtodt@gmail.com>
2023-11-05 12:01:24 -0300

    validate uuid before using it

--- src/registries/registries.service.ts
+++ src/registries/registries.service.ts
@@ -2,12 +2,17 @@ import { Inject, Injectable } from '@nestjs/common';
 import { UpdateRegistryDto } from './dto/update-registry.dto';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
+import { UuidValidator } from 'src/validators/uuid-validator/uuid-validator';
 
 @Injectable()
 export class RegistriesService {
-  constructor(@Inject(CACHE_MANAGER) private cacheService: Cache) {}
+  constructor(
+    @Inject(CACHE_MANAGER) private cacheService: Cache,
+    private uuidValidator: UuidValidator
+  ) {}
 
   save(registry: UpdateRegistryDto) {
+    if(!this.uuidValidator.isUuidv4(registry.key)) return;
     this.cacheService.set(registry.key, registry.data, {
       // 3600000ml = 1h
       ttl: new Blob([registry.data]).size * 3600000
@@ -15,6 +20,7 @@ export class RegistriesService {
   }
 
   obtainWith(key: string) {
+    if(!this.uuidValidator.isUuidv4(key)) throw new Error("Invalid v4 UUID!");
     return this.cacheService.get(key);
   }
 }

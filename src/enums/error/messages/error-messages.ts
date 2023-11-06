c7f419a61dbc852de9857494e0151bd088ffc9f3
Toodie <lucas.vtodt@gmail.com>
2023-11-06 11:06:44 -0300

    add enums for error messages

---
+++ src/enums/error/messages/error-messages.ts
@@ -0,0 +1,5 @@
+export enum ErrorMessages {
+    INVALID_UUID = "The uuid is not valid for this api!",
+    INVALID_DATA = "The given data is not valid for this api!",
+    UNKNONW      = "Can`t provide better information!"
+}

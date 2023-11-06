a1bdc6b879e044275648178cdc300c1ea5ecdb25
Toodie <lucas.vtodt@gmail.com>
2023-11-06 11:20:30 -0300

    add error info model for external usage

---
+++ src/models/errors/external/error-info/error-info.ts
@@ -0,0 +1,15 @@
+import { ErrorSubjects } from 'src/enums/error/subjects/error-subjects';
+import { ErrorMessages } from 'src/enums/error/messages/error-messages';
+import { ErrorSugestions } from 'src/enums/error/sugestions/error-sugestions';
+
+export class ErrorInfo {
+    private readonly subject: ErrorSubjects
+    private readonly message: ErrorMessages
+    private readonly sugestions: Array<ErrorSugestions>
+
+    constructor(subject: ErrorSubjects, message: ErrorMessages, sugestions: Array<ErrorSugestions>) {
+        this.subject    = subject;
+        this.message    = message;
+        this.sugestions = sugestions;
+    }
+}

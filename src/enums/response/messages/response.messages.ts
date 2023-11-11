48c94a8d21a8d12ed8ca13679b74e39b72c6875f
Toodie <lucas.vtodt@gmail.com>
2023-11-11 07:49:43 -0300

    add new response to enum ResponseMessages
    This message is for new succesfully created tokens.

--- src/enums/response/messages/response.messages.ts
+++ src/enums/response/messages/response.messages.ts
@@ -2,5 +2,6 @@ export enum ResponseMessages {
     DATA_RETRIEVED = "Data was succesfully retrieved!",
     DATA_NOT_RETRIEVED = "Data was NOT succesfully retrieved!",
     DATA_SAVED = "Data was succesfully saved!",
-    DATA_NOT_SAVED = "Data was NOT succesfully saved!"
+    DATA_NOT_SAVED = "Data was NOT succesfully saved!",
+    TOKEN_CREATED = "Token was created!"
 }

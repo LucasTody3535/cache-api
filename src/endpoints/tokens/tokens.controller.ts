e9e14a4daa999f209c6e62ab8d5e0b4a077aba83
Toodie <lucas.vtodt@gmail.com>
2023-11-11 08:26:51 -0300

    update getToken to encrypt the token generated before returning

--- src/endpoints/tokens/tokens.controller.ts
+++ src/endpoints/tokens/tokens.controller.ts
@@ -3,22 +3,25 @@ import { TokensService } from './tokens.service';
 import { ResponseGeneratorUtil } from 'src/utils/response-generator/response-generator.util';
 import { ResponseMessages } from 'src/enums/response/messages/response.messages';
 import { Response } from 'src/models/response/response';
+import { EncryptionService } from 'src/utils/encryption/encryption.service';
 
 @Controller('tokens')
 export class TokensController {
   constructor(
     private readonly tokensService: TokensService,
-    private readonly responseGeneratorUtil: ResponseGeneratorUtil
+    private readonly responseGeneratorUtil: ResponseGeneratorUtil,
+    private readonly encryptionService: EncryptionService
   ) {}
 
   @Get()
   @HttpCode(HttpStatus.CREATED)
   getToken(): Response<string> {
+    this.encryptionService.setup();
     return this.responseGeneratorUtil
         .genGenericResponse(
           HttpStatus.CREATED,
           ResponseMessages.TOKEN_CREATED,
-          this.tokensService.generateUniqueToken()
+          this.encryptionService.encrypt(this.tokensService.generateUniqueToken())
         ) as Response<string>;
   }
 }
--- src/endpoints/tokens/tokens.module.ts
+++ src/endpoints/tokens/tokens.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TokensService } from './tokens.service';
 import { TokensController } from './tokens.controller';
 import { ResponseGeneratorUtil } from 'src/utils/response-generator/response-generator.util';
+import { EncryptionService } from 'src/utils/encryption/encryption.service';
 
 @Module({
   controllers: [TokensController],
-  providers: [TokensService, ResponseGeneratorUtil],
+  providers: [TokensService, ResponseGeneratorUtil, EncryptionService],
 })
 export class TokensModule {}

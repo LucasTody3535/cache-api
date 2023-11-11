be7d3eecad2c2b1a01e38cb03df16ec19981fff1
Toodie <lucas.vtodt@gmail.com>
2023-11-11 07:51:29 -0300

    update getTokens to return an object Response<string>

--- src/endpoints/tokens/tokens.controller.ts
+++ src/endpoints/tokens/tokens.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { TokensService } from './tokens.service';
+import { ResponseGeneratorUtil } from 'src/utils/response-generator/response-generator.util';
+import { ResponseMessages } from 'src/enums/response/messages/response.messages';
+import { Response } from 'src/models/response/response';
 
 @Controller('tokens')
 export class TokensController {
-  constructor(private readonly tokensService: TokensService) {}
+  constructor(
+    private readonly tokensService: TokensService,
+    private readonly responseGeneratorUtil: ResponseGeneratorUtil
+  ) {}
 
   @Get()
-  getToken(): string {
-    return this.tokensService.generateUniqueToken();
+  @HttpCode(HttpStatus.CREATED)
+  getToken(): Response<string> {
+    return this.responseGeneratorUtil
+        .genGenericResponse(
+          HttpStatus.CREATED,
+          ResponseMessages.TOKEN_CREATED,
+          this.tokensService.generateUniqueToken()
+        ) as Response<string>;
   }
 }
--- src/endpoints/tokens/tokens.module.ts
+++ src/endpoints/tokens/tokens.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TokensService } from './tokens.service';
 import { TokensController } from './tokens.controller';
+import { ResponseGeneratorUtil } from 'src/utils/response-generator/response-generator.util';
 
 @Module({
   controllers: [TokensController],
-  providers: [TokensService],
+  providers: [TokensService, ResponseGeneratorUtil],
 })
 export class TokensModule {}

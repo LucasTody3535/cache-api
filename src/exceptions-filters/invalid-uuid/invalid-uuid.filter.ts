eeaff70019a5ff8776c24c21ad253a995c278832
Toodie <lucas.vtodt@gmail.com>
2023-11-11 09:13:26 -0300

    add exception filter for invalid uuid errors

---
+++ src/exceptions-filters/invalid-uuid/invalid-uuid.filter.ts
@@ -0,0 +1,25 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from '@nestjs/common';
+import { ResponseMessages } from 'src/enums/response/messages/response.messages';
+import { ResponseGeneratorUtil } from 'src/utils/response-generator/response-generator.util';
+import { Response } from 'express';
+
+@Catch(Error)
+export class InvalidUuidFilter<T> implements ExceptionFilter {
+  constructor(private responseGeneratorUtil: ResponseGeneratorUtil) {}
+
+  catch(exception: Error, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    response
+      .status(HttpStatus.FORBIDDEN)
+      .json(
+        this.responseGeneratorUtil
+            .genGenericResponse(
+              HttpStatus.FORBIDDEN,
+              ResponseMessages.DATA_NOT_SAVED,
+              exception.message
+            )
+        );
+  }
+}

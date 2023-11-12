da2f8e68c73ed7f8f0aee3c0120af25dca956975
Toodie <lucas.vtodt@gmail.com>
2023-11-12 00:31:00 -0300

    decrypt key before saving into cache

--- src/endpoints/registries/registries.controller.ts
+++ src/endpoints/registries/registries.controller.ts
@@ -5,18 +5,22 @@ import { Response } from 'src/models/response/response';
 import { ResponseGeneratorUtil } from 'src/utils/response-generator/response-generator.util';
 import { ResponseMessages } from 'src/enums/response/messages/response.messages';
 import { InvalidUuidFilter } from 'src/exceptions-filters/invalid-uuid/invalid-uuid.filter';
+import { EncryptionService } from 'src/utils/encryption/encryption.service';
 import { TokenErrorFilter } from 'src/filters/token-error/token-error.filter';
 
 @Controller('registries')
 export class RegistriesController {
   constructor(
     private readonly registriesService: RegistriesService,
-    private responseGeneratorUtil: ResponseGeneratorUtil
+    private responseGeneratorUtil: ResponseGeneratorUtil,
+    private readonly encryptionService: EncryptionService
   ) {}
 
   @Post()
   @UseFilters(TokenErrorFilter)
   updateRegistry(@Body() registry: UpdateRegistryDto) {
+    this.encryptionService.setup();
+    registry.key = this.encryptionService.decrypt(registry.key);
     this.registriesService.save(registry);
     return this.responseGeneratorUtil
       .genGenericResponse(

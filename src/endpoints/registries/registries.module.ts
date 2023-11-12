37e18dc36276fa80a2981568924ef415ec943f4e
Toodie <lucas.vtodt@gmail.com>
2023-11-12 00:37:15 -0300

    import encryption service as a provider in RegistriesModule

--- src/endpoints/registries/registries.module.ts
+++ src/endpoints/registries/registries.module.ts
@@ -3,9 +3,10 @@ import { RegistriesService } from './registries.service';
 import { RegistriesController } from './registries.controller';
 import { UuidValidator } from 'src/validators/uuid-validator/uuid-validator';
 import { ResponseGeneratorUtil } from 'src/utils/response-generator/response-generator.util';
+import { EncryptionService } from 'src/utils/encryption/encryption.service';
 
 @Module({
   controllers: [RegistriesController],
-  providers: [RegistriesService, UuidValidator, ResponseGeneratorUtil],
+  providers: [RegistriesService, UuidValidator, ResponseGeneratorUtil, EncryptionService],
 })
 export class RegistriesModule {}

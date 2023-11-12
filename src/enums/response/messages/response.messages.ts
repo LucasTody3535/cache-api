6c72c96d5656d3db83ed1f1dc2d9405b07b2b346
Toodie <lucas.vtodt@gmail.com>
2023-11-12 00:40:43 -0300

    add COULD_NOT_DO_OPERATION to enum ResponseMessages

--- src/enums/response/messages/response.messages.ts
+++ src/enums/response/messages/response.messages.ts
@@ -1,4 +1,5 @@
 export enum ResponseMessages {
+    COULD_NOT_DO_OPERATION = "The requested operation could not be done",
     DATA_RETRIEVED = "Data was succesfully retrieved!",
     DATA_NOT_RETRIEVED = "Data was NOT succesfully retrieved!",
     DATA_SAVED = "Data was succesfully saved!",

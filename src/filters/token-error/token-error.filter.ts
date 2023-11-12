c885f27b79142e692149e41340ba5493beb11942
Toodie <lucas.vtodt@gmail.com>
2023-11-12 02:55:42 -0300

    make token error filter catch UuidError too

--- src/filters/token-error/token-error.filter.ts
+++ src/filters/token-error/token-error.filter.ts
@@ -6,8 +6,9 @@ import { ErrorInfo } from 'src/models/errors/external/error-info/error-info';
 import { ErrorSubjects } from 'src/enums/error/subjects/error-subjects';
 import { ErrorMessages } from 'src/enums/error/messages/error-messages';
 import { TokenError } from 'src/models/errors/internal/token-error/token-error';
+import { UuidError } from 'src/models/errors/internal/uuid-error/uuid-error';
 
-@Catch(TokenError)
+@Catch(TokenError, UuidError)
 export class TokenErrorFilter<T> implements ExceptionFilter {
   constructor(private responseService: ResponseService) {}
 

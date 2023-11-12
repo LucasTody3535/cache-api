6c9c96e0e861b795b07503c3b388b838a3e85bb4
Toodie <lucas.vtodt@gmail.com>
2023-11-11 23:23:39 -0300

    add genErrorResponse method to ResponseGeneratorUtil class

--- src/utils/response-generator/response-generator.util.ts
+++ src/utils/response-generator/response-generator.util.ts
@@ -1,10 +1,15 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { ResponseMessages } from 'src/enums/response/messages/response.messages';
+import { ErrorInfo } from 'src/models/errors/external/error-info/error-info';
 import { Response } from 'src/models/response/response';
 
 @Injectable()
 export class ResponseGeneratorUtil {
     genGenericResponse(httpStatus: HttpStatus, message: ResponseMessages, payload: unknown) {
         return new Response<unknown>(httpStatus, message, payload);
+    }
+
+    genErrorResponse(httpStatus: HttpStatus, message: ResponseMessages, errorInfo: ErrorInfo) {
+        return new Response<ErrorInfo>(httpStatus, message, errorInfo);
     }
 }

9edbabf69bfb5bfda5734bd785f1e2e51bea1545
Toodie <lucas.vtodt@gmail.com>
2023-11-11 23:41:29 -0300

    simplify UuidError class

--- src/models/errors/internal/uuid-error/uuid-error.ts
+++ src/models/errors/internal/uuid-error/uuid-error.ts
@@ -1,9 +1,5 @@
 export class UuidError extends Error {
-    static is(obj: Object) {
-        return obj instanceof UuidError;
-    }
-
-    constructor(message: string) {
-        super(message);
+    constructor() {
+        super();
     }
 }

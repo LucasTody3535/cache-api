a809fff74a36b0e5569fd80dc075858716c08603
Toodie <lucas.vtodt@gmail.com>
2023-11-12 02:54:15 -0300

    remove uuid validation of all methods in RegistriesService

--- src/endpoints/registries/registries.service.ts
+++ src/endpoints/registries/registries.service.ts
@@ -2,17 +2,12 @@ import { Inject, Injectable } from '@nestjs/common';
 import { UpdateRegistryDto } from './dto/update-registry.dto';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
-import { UuidValidator } from 'src/validators/uuid-validator/uuid-validator';
 
 @Injectable()
 export class RegistriesService {
-  constructor(
-    @Inject(CACHE_MANAGER) private cacheService: Cache,
-    private uuidValidator: UuidValidator
-  ) {}
+  constructor(@Inject(CACHE_MANAGER) private cacheService: Cache) {}
 
   save(registry: UpdateRegistryDto, key: string) {
-    if(!this.uuidValidator.isUuidv4(key)) throw new Error("Invalid v4 UUID!");
     this.cacheService.set(key, registry.data, {
       // 3600000ml = 1h
       ttl: new Blob([registry.data]).size * 3600000
@@ -20,7 +15,6 @@ export class RegistriesService {
   }
 
   obtainWith(key: string) {
-    if(!this.uuidValidator.isUuidv4(key)) throw new Error("Invalid v4 UUID!");
     return this.cacheService.get(key);
   }
 }

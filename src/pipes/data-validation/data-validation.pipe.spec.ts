7d8945c96c1bef6ec5d280a733d1b88f59eb99b6
Toodie <lucas.vtodt@gmail.com>
2023-11-12 06:04:42 -0300

    add data validation pipe

---
+++ src/pipes/data-validation/data-validation.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { DataValidationPipe } from './data-validation.pipe';
+
+describe('DataValidationPipe', () => {
+  it('should be defined', () => {
+    expect(new DataValidationPipe()).toBeDefined();
+  });
+});

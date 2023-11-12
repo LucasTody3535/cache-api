a322958af9b26234e774b492186e304db989aa23
Toodie <lucas.vtodt@gmail.com>
2023-11-12 06:10:24 -0300

    add swagger docs to tokens controller

--- src/endpoints/tokens/tokens.controller.ts
+++ src/endpoints/tokens/tokens.controller.ts
@@ -4,6 +4,7 @@ import { ResponseService } from 'src/utils/response/response.service';
 import { ResponseMessages } from 'src/enums/response/messages/response.messages';
 import { Response } from 'src/models/response/response';
 import { EncryptionService } from 'src/utils/encryption/encryption.service';
+import { ApiCreatedResponse, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('tokens')
 export class TokensController {
@@ -13,6 +14,9 @@ export class TokensController {
     private readonly encryptionService: EncryptionService
   ) {}
 
+  @ApiOperation({ summary: 'Used to obtain an encrypted cache key, used for cache operations' })
+  @ApiResponse({ status: 201, description: "The token was created succesfully" })
+  @ApiCreatedResponse({ description: 'The cache key was created and returned', schema: { example: new Response(HttpStatus.CREATED, ResponseMessages.TOKEN_CREATED, "3039a6f03f265d49509917f3e6caa68d") } })
   @Get()
   @HttpCode(HttpStatus.CREATED)
   getToken(): Response<string> {

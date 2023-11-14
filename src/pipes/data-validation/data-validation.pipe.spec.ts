05a6311e42f76134858833077f6c47d044071e16
Toodie <lucas.vtodt@gmail.com>
2023-11-14 09:11:53 -0300

    remove old pipe related to data validation

--- src/pipes/data-validation/data-validation.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DataValidationPipe } from './data-validation.pipe';
-
-describe('DataValidationPipe', () => {
-  it('should be defined', () => {
-    expect(new DataValidationPipe()).toBeDefined();
-  });
-});

7677f7d19c64ce826c37e2d1bad5905a171cd685
Toodie <lucas.vtodt@gmail.com>
2023-11-14 10:52:10 -0300

    add pipe that validates the size of the registry to be saved

---
+++ src/pipes/registry-data-size-validator/registry-data-size-validator.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { RegistryDataSizeValidatorPipe } from './registry-data-size-validator.pipe';
+
+describe('RegistryDataSizeValidatorPipe', () => {
+  it('should be defined', () => {
+    expect(new RegistryDataSizeValidatorPipe()).toBeDefined();
+  });
+});
---
+++ src/pipes/registry-data-size-validator/registry-data-size-validator.pipe.ts
@@ -0,0 +1,14 @@
+import { Injectable, PipeTransform } from '@nestjs/common';
+import { UpdateRegistryDto } from 'src/endpoints/registries/dto/update-registry.dto';
+import { RegistryDataSizeError } from 'src/models/errors/internal/registry-data-size-error/registry-data-size-error';
+
+@Injectable()
+export class RegistryDataSizeValidatorPipe implements PipeTransform {
+  constructor(private readonly maxSizeInBytes: number) {}
+
+  transform(registry: UpdateRegistryDto, _: never) {
+    const registryBlob = new Blob([registry.data]);
+    if(registryBlob.size > this.maxSizeInBytes) throw new RegistryDataSizeError();
+    return registry;
+  }
+}

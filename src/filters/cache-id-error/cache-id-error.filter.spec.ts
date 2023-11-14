80587a75c585b0b4c293e664ee54d244308e424e
Toodie <lucas.vtodt@gmail.com>
2023-11-14 10:18:35 -0300

    add filter for cache id related errors

---
+++ src/filters/cache-id-error/cache-id-error.filter.spec.ts
@@ -0,0 +1,7 @@
+import { CacheIdErrorFilter } from './cache-id-error.filter';
+
+describe('CacheIdErrorFilter', () => {
+  it('should be defined', () => {
+    expect(new CacheIdErrorFilter()).toBeDefined();
+  });
+});
---
+++ src/filters/cache-id-error/cache-id-error.filter.ts
@@ -0,0 +1,31 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from '@nestjs/common';
+import { ResponseMessages } from 'src/enums/response/messages/response.messages';
+import { ResponseService } from 'src/utils/response/response.service';
+import { Request, Response } from 'express';
+import { ErrorInfo } from 'src/models/errors/external/error-info/error-info';
+import { ErrorSubjects } from 'src/enums/error/subjects/error-subjects';
+import { ErrorMessages } from 'src/enums/error/messages/error-messages';
+import { CacheIdError } from 'src/models/errors/internal/cache-id-error/cache-id-error';
+
+@Catch(CacheIdError)
+export class CacheIdErrorFilter<T> implements ExceptionFilter {
+  constructor(private responseService: ResponseService) {}
+
+  catch(_: never, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const errorInfo = new ErrorInfo(ErrorSubjects.CACHE_ID, ErrorMessages.CACHE_ID, [], request.path);
+
+    response
+      .status(HttpStatus.BAD_REQUEST)
+      .json(
+        this.responseService
+            .genErrorResponse(
+              HttpStatus.BAD_REQUEST,
+              ResponseMessages.COULD_NOT_DO_OPERATION,
+              errorInfo
+            )
+        );
+  }
+}

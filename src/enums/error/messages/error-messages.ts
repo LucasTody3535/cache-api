122bedc1cbab8c3a7992e2264681649ad3968f66
Toodie <lucas.vtodt@gmail.com>
2023-11-14 11:02:53 -0300

    add error message enum for registry that exceeds the max size permitted

--- src/enums/error/messages/error-messages.ts
+++ src/enums/error/messages/error-messages.ts
@@ -1,6 +1,7 @@
 export enum ErrorMessages {
     INVALID_TOKEN = "The token is not valid for this api!",
     INVALID_DATA = "The given data is not valid for this api!",
+    REGISTRY_TOO_LARGE = "The data to be stored exceeds the maximum permitted!",
     UNKNONW      = "Can`t provide better information!",
     CACHE_ID     = "The given cache id is not valid!"
 }

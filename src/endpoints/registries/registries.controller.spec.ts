bf014238317cd3f2f68525f8daf4437ac9cb047a
Toodie <lucas.vtodt@gmail.com>
2023-11-15 12:30:03 -0300

    add missing modules in registries controller test file

--- src/endpoints/registries/registries.controller.spec.ts
+++ src/endpoints/registries/registries.controller.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RegistriesController } from './registries.controller';
 import { RegistriesService } from './registries.service';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { CacheModule } from '@nestjs/cache-manager';
 
 describe('RegistriesController', () => {
   let controller: RegistriesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CacheModule.register(), ThrottlerModule.forRoot([
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ])],
       controllers: [RegistriesController],
       providers: [RegistriesService],
     }).compile();

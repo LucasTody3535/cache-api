8e4794abbd242c72d8449b4590793573d6a2fd8a
Toodie <lucas.vtodt@gmail.com>
2023-11-15 12:16:04 -0300

    replace absolute import with relative import in data size validator

--- src/pipes/registry-data-size-validator/registry-data-size-validator.pipe.ts
+++ src/pipes/registry-data-size-validator/registry-data-size-validator.pipe.ts
@@ -1,6 +1,6 @@
 import { Injectable, PipeTransform } from '@nestjs/common';
-import { UpdateRegistryDto } from 'src/endpoints/registries/dto/update-registry.dto';
-import { RegistryDataSizeError } from 'src/models/errors/internal/registry-data-size-error/registry-data-size-error';
+import { UpdateRegistryDto } from '../../endpoints/registries/dto/update-registry.dto';
+import { RegistryDataSizeError } from '../../models/errors/internal/registry-data-size-error/registry-data-size-error';
 
 @Injectable()
 export class RegistryDataSizeValidatorPipe implements PipeTransform {

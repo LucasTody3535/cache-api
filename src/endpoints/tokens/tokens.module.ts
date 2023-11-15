0e4c11fa29a06bc8fa578415048864cc44752a6a
Toodie <lucas.vtodt@gmail.com>
2023-11-15 12:14:19 -0300

    replace absolute import with relative import in tokens module

--- src/endpoints/tokens/tokens.module.ts
+++ src/endpoints/tokens/tokens.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TokensService } from './tokens.service';
 import { TokensController } from './tokens.controller';
-import { ResponseService } from 'src/utils/response/response.service';
-import { EncryptionService } from 'src/utils/encryption/encryption.service';
+import { ResponseService } from '../../utils/response/response.service';
+import { EncryptionService } from '../../utils/encryption/encryption.service';
 
 @Module({
   controllers: [TokensController],

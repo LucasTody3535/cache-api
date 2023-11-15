ffa90badeac289bee2a6dc7dd566c6286ed1444a
Toodie <lucas.vtodt@gmail.com>
2023-11-15 12:15:23 -0300

    replace absolute import with relative import in data struc. error filter

--- src/filters/registry-data-structure-error/registry-data-structure-error.filter.ts
+++ src/filters/registry-data-structure-error/registry-data-structure-error.filter.ts
@@ -4,13 +4,13 @@ import {
   ExceptionFilter,
   HttpStatus,
 } from '@nestjs/common';
-import { ResponseMessages } from 'src/enums/response/messages/response.messages';
-import { ResponseService } from 'src/utils/response/response.service';
+import { ResponseMessages } from '../../enums/response/messages/response.messages';
+import { ResponseService } from '../../utils/response/response.service';
 import { Request, Response } from 'express';
-import { ErrorInfo } from 'src/models/errors/external/error-info/error-info';
-import { ErrorSubjects } from 'src/enums/error/subjects/error-subjects';
-import { ErrorMessages } from 'src/enums/error/messages/error-messages';
-import { RegistryDataStructureError } from 'src/models/errors/internal/registry-data-structure-error/registry-data-structure-error';
+import { ErrorInfo } from '../../models/errors/external/error-info/error-info';
+import { ErrorSubjects } from '../../enums/error/subjects/error-subjects';
+import { ErrorMessages } from '../../enums/error/messages/error-messages';
+import { RegistryDataStructureError } from '../../models/errors/internal/registry-data-structure-error/registry-data-structure-error';
 
 @Catch(RegistryDataStructureError)
 export class RegistryDataStructureErrorFilter<T> implements ExceptionFilter {

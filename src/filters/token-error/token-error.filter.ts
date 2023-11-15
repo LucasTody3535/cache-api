aa48c463608078024a8270047e068b4ec3b090b6
Toodie <lucas.vtodt@gmail.com>
2023-11-15 12:15:32 -0300

    replace absolute import with relative import in token error filter

--- src/filters/token-error/token-error.filter.ts
+++ src/filters/token-error/token-error.filter.ts
@@ -4,14 +4,14 @@ import {
   ExceptionFilter,
   HttpStatus,
 } from '@nestjs/common';
-import { ResponseMessages } from 'src/enums/response/messages/response.messages';
-import { ResponseService } from 'src/utils/response/response.service';
+import { ResponseMessages } from '../../enums/response/messages/response.messages';
+import { ResponseService } from '../../utils/response/response.service';
 import { Request, Response } from 'express';
-import { ErrorInfo } from 'src/models/errors/external/error-info/error-info';
-import { ErrorSubjects } from 'src/enums/error/subjects/error-subjects';
-import { ErrorMessages } from 'src/enums/error/messages/error-messages';
-import { TokenError } from 'src/models/errors/internal/token-error/token-error';
-import { UuidError } from 'src/models/errors/internal/uuid-error/uuid-error';
+import { ErrorInfo } from '../../models/errors/external/error-info/error-info';
+import { ErrorSubjects } from '../../enums/error/subjects/error-subjects';
+import { ErrorMessages } from '../../enums/error/messages/error-messages';
+import { TokenError } from '../../models/errors/internal/token-error/token-error';
+import { UuidError } from '../../models/errors/internal/uuid-error/uuid-error';
 
 @Catch(TokenError, UuidError)
 export class TokenErrorFilter<T> implements ExceptionFilter {
